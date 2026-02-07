6b076ca9c4a233068b4a3286e7914aa60434be4f
Sebastian Sandstig <sebastian.sandstig@gmail.com>
2026-02-07 15:30:34 +0100

    title

--- app/root.tsx
+++ app/root.tsx
@@ -58,6 +58,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <title>Langlog</title>
         <Links />
       </head>
       <body>
